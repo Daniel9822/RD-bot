765e8a98af9c54da8de3dff880a03ed3076b9050
Daniel9822 <dhdany@gmail.com>
2021-05-08 18:24:35 -0400

    First commit

---
+++ welcomtext.js
@@ -0,0 +1,9 @@
+const welcome = (number, groupname) => {
+    return `𝗛𝗼𝗹𝗮 @${number}. 𝐒𝐞𝐚 𝗯𝗶𝗲𝗻𝘃𝗲𝗻𝗶𝗱𝗼 𝐚𝗹 𝐠𝐫𝐮𝐩𝐨 ${groupname}`
+}
+exports.welcome = welcome
+
+const bye = (number) => {
+    return `Adios @${number}. ya era hora deque te fueras idiota👋`
+}
+exports.bye = bye
